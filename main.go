372f9c474802604f200702be6f813b7bf5ef1d68
alfredo-vive <fredyyanez39@yahoo.com>
2023-09-22 11:15:48 +0200

    better documenting

--- main.go
+++ main.go
@@ -21,33 +21,36 @@ type Application struct {
 
 func (app *Application) Serve() error {
 	port := app.Config.Port
+	fmt.Printf("Serving app on port %s", port)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
-		Handler: router.Routes(),
+		Handler: router.Routes(), // This handles the routing
 	}
 	return srv.ListenAndServe()
 }
 
 func main() {
+	// Load ENV variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	// Handle DB connection
 	db, err := database.ConnectDB()
 	if err != nil {
 		log.Fatal("Cannot connect to database")
 	}
-
+	// Defer disconnect until program stops
 	defer db.Client.Disconnect()
 
 	config := Config{
 		Port: os.Getenv("PORT"),
 	}
-
+	// Create App
 	app := &Application{
 		Config: config,
 	}
-
+	// Serve and Listen
 	err = app.Serve()
 	if err != nil {
 		log.Fatal(err)
